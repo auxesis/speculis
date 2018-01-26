7e3574825817c44c4eec2f464858365a8914a90b
Lindsay Holmwood <lindsay@holmwood.id.au>
2018-01-27 02:03:43 +1100

    Add scrapers/runners for top Golang projects on GitHub

---
+++ cf-cli/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+alias readlink=greadlink
+
+BASEDIR="$(readlink -f $(pwd)/$(dirname $0)/..)"
+ARTIFACT_DIR="$BASEDIR/artifacts"
+REPO="github.com/cloudfoundry/cli"
+NORMALISED_REPO="$(echo $REPO | sed 's/\//_/g')"
+DOTFILE="$NORMALISED_REPO.dot"
+PDF="$NORMALISED_REPO.pdf"
+IGNORE_PREFIXES="$REPO/vendor/"
+
+# Fetch
+export GOPATH="$HOME"
+go get -u -v $REPO
+
+# Graph
+cd $GOPATH/src/$REPO
+godepgraph -s $REPO | tee $DOTFILE
+
+# Visualise
+cat $DOTFILE | dot -Tpdf -o $PDF
+
+# Store artifacts
+cp $DOTFILE $ARTIFACT_DIR
+cp $PDF $ARTIFACT_DIR
